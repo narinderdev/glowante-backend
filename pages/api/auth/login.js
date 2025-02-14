019532d183850632d297d85f6aced80954d52e9e
Narinder Dev <narinder.dev@gmail.com>
2025-02-14 19:29:04 +0530

    Commentted unused code

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -2,7 +2,7 @@ import db from '../../../lib/db';
 import { sendResponse } from '../../../lib/response';
 import corsMiddleware from '../../../lib/cors';
 // import { generateOTP } from '../../../lib/otp'; // Utility to generate OTP
-import axios from 'axios'; // For sending SMS via Fast2SMS
+// import axios from 'axios'; // For sending SMS via Fast2SMS
 import dotenv from 'dotenv';
 
 dotenv.config();
