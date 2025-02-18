70d252a2c5a8f52232c9a698a62fddfb90e36934
Narinder Dev <narinder.dev@gmail.com>
2025-02-18 18:44:22 +0530

    Used default response

--- pages/api/users/get-all-users.js
+++ pages/api/users/get-all-users.js
@@ -1,36 +1,39 @@
 import db from '../../../lib/db';
+import { sendResponse } from '../../../lib/response';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const users = await db.any(`
-        SELECT 
-          u.id AS user_id,
-          u.first_name,
-          u.last_name,
-          u.email,
-          u.country_code,
-          u.phone_number,
-          u.profile_picture_url,
-          u.status AS user_status,
-          r.role_name,
-          ua.city,
-          ua.state,
-          ua.zipcode,
-          ua.street,
-          ua.status AS address_status
-        FROM users u
-        LEFT JOIN user_roles ur ON u.id = ur.user_id
-        LEFT JOIN roles r ON ur.role_id = r.role_id
-        LEFT JOIN user_addresses ua ON u.id = ua.user_id
-      `);
-
-      return res.status(200).json(users);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      return res.status(500).json({ message: 'Internal Server Error' });
+    if (req.method === 'GET') {
+      try {
+        const users = await db.any(`
+          SELECT 
+            u.id AS user_id,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.country_code,
+            u.phone_number,
+            u.profile_picture_url,
+            u.status AS user_status,
+            r.role_name,
+            ua.city,
+            ua.state,
+            ua.zipcode,
+            ua.street,
+            ua.status AS address_status
+          FROM users u
+          LEFT JOIN user_roles ur ON u.id = ur.user_id
+          LEFT JOIN roles r ON ur.role_id = r.role_id
+          LEFT JOIN user_addresses ua ON u.id = ua.user_id
+        `);
+  
+        // Use the sendResponse function to send a consistent response
+        return sendResponse(res, true, users);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        // Use sendResponse for error handling as well
+        return sendResponse(res, false, {}, 'Internal Server Error', 500);
+      }
+    } else {
+      return sendResponse(res, false, {}, 'Method Not Allowed', 405);
     }
-  } else {
-    return res.status(405).json({ message: 'Method Not Allowed' });
   }
-}
